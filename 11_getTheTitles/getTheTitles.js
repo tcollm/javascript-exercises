3f43d4a6983175c1c47e63fade54d699a5e7d528
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 12:28:27 -0600

    Create getTheTitles function to parse an array and get all of the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(...args) {
+    let titleArray = []; 
+    for (let i = 0; i < args.length; i++) {
+        titleArray += args[i].title; 
+    }
+    return titleArray; 
 };
 
 // Do not edit below this line
