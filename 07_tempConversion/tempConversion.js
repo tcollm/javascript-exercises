8e6a2f07ddc56a5aa2c73fa8a30c7d941188e6ee
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 11:01:06 -0600

    Create convertToFahrenheit function for C to F calculations

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// Formula for F to C: (x - 32) * 5/9
+const convertToCelsius = function(temp) {
+  let convertedTemp = ((temp - 32) * 5/9);
+  
+  return Math.round((convertedTemp + Number.EPSILON) * 100) / 100;  
 };
 
-const convertToFahrenheit = function() {
+// Forumla for C to FL x * 5/9 + 32
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = temp * 5/9 + 32; 
+
+  return Math.round((convertedTemp + Number.EPSILON) * 100) / 100;  
 };
 
 // Do not edit below this line
