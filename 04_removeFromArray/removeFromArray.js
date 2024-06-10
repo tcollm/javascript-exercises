22f7ab81f1662c17f7f2ed38ad87e52c10c2f6c9
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-09 23:39:30 -0600

    Create removeFromArray function for multiple remove values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeValue) {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === removeValue) {
+            count++; 
+        }
+    }
+
+    if (count === 0) {
+        return array; 
+    }
+
+    let fixedArray = []; 
+    for (let i = 0; i < count; i++) {
+        let lenFixedArray = 0; 
+        if (array[i] != removeValue) {
+            fixedArray[lenFixedArray] += array[i]; 
+            lenFixedArray++;
+        }
+    }
+
+    return fixedArray; 
 };
 
 // Do not edit below this line
