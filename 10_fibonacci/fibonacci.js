c96945ad4fc8990c4a1308367be6deb7e711a24b
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 12:10:30 -0600

    Create fibonacci function to generate the nth term in the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+// fibonacci sequence = 1,1,2,3,5,8,13,...
+const fibonacci = function(nthTerm) {
+    let lastTerm = 0; 
+    let currentTerm = 1; 
+    let fibonacciVal = 0; 
 
+    for (let i = 0; i < nthTerm; i++) {
+        currentTerm += lastTerm;
+        lastTerm = currentTerm; 
+        fibonacciVal += currentTerm; 
+    }
+    return fibonacciVal; 
 };
 
 // Do not edit below this line
