8ba1942be63b58446714ac4a52d39bcb5ef603dd
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 10:34:37 -0600

    Create sumAll function to add all numberss from given min to max values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = min;
+    for (let i = min + 1; i < max + 1; i++) {
+        sum += i; 
+    }
+    return sum; 
 };
 
 // Do not edit below this line
