0784348b0f99cf304c1f95ac3ff575abf9f3a5b9
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 10:40:22 -0600

    Change arguments to num1 and num2, and then assign min and max to the lesser and greater values respectively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function(min, max) {
+const sumAll = function(num1, num2) {
+    let min = 0;
+    let max = 0; 
+    if (num2 > num1) {
+        max = num2; 
+        min = num1; 
+    } else {
+        max = num1;
+        min = num2; 
+    }
+
     let sum = min;
     for (let i = min + 1; i < max + 1; i++) {
         sum += i; 
