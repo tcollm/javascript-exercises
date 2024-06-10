14cdfa52750ab4edb2ed3cc56d859a49d0817014
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 11:02:37 -0600

    Change both functions to return a single decimal float instead of a float with two decimal places

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,14 +2,14 @@
 const convertToCelsius = function(temp) {
   let convertedTemp = ((temp - 32) * 5/9);
   
-  return Math.round((convertedTemp + Number.EPSILON) * 100) / 100;  
+  return Math.round((convertedTemp + Number.EPSILON) * 10) / 10;  
 };
 
 // Forumla for C to FL x * 5/9 + 32
 const convertToFahrenheit = function(temp) {
   let convertedTemp = temp * 5/9 + 32; 
 
-  return Math.round((convertedTemp + Number.EPSILON) * 100) / 100;  
+  return Math.round((convertedTemp + Number.EPSILON) * 10) / 10;  
 };
 
 // Do not edit below this line
