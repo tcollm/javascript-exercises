ec1136176c3ea46a3302b80ece7803394b1f473e
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-09 23:20:38 -0600

    Create repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = string;
+    for (let i = 1; i < num; i++) {
+        returnString += string; 
+    }
+    return returnString; 
 };
 
 // Do not edit below this line
