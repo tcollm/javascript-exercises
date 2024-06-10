907922ac94b247088e330d2a48bdcb4547f4da20
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-09 23:43:41 -0600

    Create removeFromArray function to remove a single element from array using the splice method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,8 @@
 const removeFromArray = function(array, removeValue) {
-    let count = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === removeValue) {
-            count++; 
-        }
-    }
+    let index = array.indexOf(removeValue);
+    array.splice(index, 1);
 
-    if (count === 0) {
-        return array; 
-    }
-
-    let fixedArray = []; 
-    for (let i = 0; i < count; i++) {
-        let lenFixedArray = 0; 
-        if (array[i] != removeValue) {
-            fixedArray[lenFixedArray] += array[i]; 
-            lenFixedArray++;
-        }
-    }
-
-    return fixedArray; 
+    return array; 
 };
 
 // Do not edit below this line
