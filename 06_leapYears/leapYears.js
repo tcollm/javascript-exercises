cf071a724837a2e34f1f652d83e90c3a84d62f2b
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 10:54:08 -0600

    Create leapYears function to check if a year is considered a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+// Years by 4 are leap years, unless they are divisible by 100, if so, then they 
+// must also be divisible by 400 to be considered a leap year. 
+const leapYears = function(year) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true; 
+        } else {
+            return false;  
+        }
+    } else if (year % 4 === 0) {
+        return true; 
+    }
 
+    return false; 
 };
 
 // Do not edit below this line
