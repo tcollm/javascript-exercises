f1fc7064f58fbdbb03ae8b8cb08b21d903409955
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 11:10:16 -0600

    Create sum function to add all numbers in arguments array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,7 +7,11 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function() {
-	
+  let sum = 0; 
+	for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return Number(sum); 
 };
 
 const multiply = function() {
