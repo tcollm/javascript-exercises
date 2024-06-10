29f9943a7a217abbc2565d70443bb35d683ba775
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 12:23:16 -0600

    Create if statement to check if 0 was passed and return 0 if so

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,14 @@
 // fibonacci sequence = 1,1,2,3,5,8,13,...
 const fibonacci = function(nthTerm) {
+    if (nthTerm === 0) {
+        return 0; 
+    }
+
     let currentTerm = 1;
     let lastTerm = 0; 
     let tempStorage = 0; 
 
     for (let i = 0; i < nthTerm - 1; i++) {
-        // console.log("Equation: " + currentTerm + " + " + lastTerm + " = " + (currentTerm+lastTerm));
         tempStorage = currentTerm; 
         currentTerm += lastTerm;
         lastTerm = tempStorage; 
