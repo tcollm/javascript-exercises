07d5230d4c1e86df366b8def3eb947bed96b32d0
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 11:50:02 -0600

    Change end of string index from string.length - i + 1 to string.length - i - 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,10 @@
 const palindromes = function (string) {
     console.log("STRING: " + string); 
     for (let i = 0; i < string.length; i++) {
-        console.log("Beginning of string: " + string[i] + " \n" + 
-                    "End of string: " + string[string.length - i + 1]); 
+        // console.log("Beginning of string: " + string[i] + " \n" + 
+        //             "End of string: " + string[string.length - 1 - i]); 
 
-        if (string[i] != string[string.length - i + 1]) {
+        if (string[i] != string[string.length - i - 1]) {
             return false; 
         }
     }
