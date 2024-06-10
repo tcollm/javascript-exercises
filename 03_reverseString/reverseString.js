701bd56d5c888f14b799684c290e0c21e5b9222a
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-09 23:33:43 -0600

    Add if condition to check if string is empty, so that an empty string is returned

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,8 @@
 const reverseString = function(string) {
+    if (string.length === 0) {
+        return "";
+    }
+
     let reverseString = string[string.length - 1];
 
     for (let i = 2; i < string.length + 1; i++) {
