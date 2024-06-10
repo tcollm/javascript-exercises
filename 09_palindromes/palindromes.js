43117d685ade5e4b7a7fe94cfab66f4d2931d508
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-10 11:43:45 -0600

    Create palindromes function to check if a given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] != string[length - i]) {
+            return false; 
+        }
+    }
+    return true; 
 };
 
 // Do not edit below this line
