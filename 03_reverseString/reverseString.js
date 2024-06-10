c28556494141e0ca6e53310731680e18dd655138
Tyler Collingridge <tylercollingridge@gmail.com>
2024-06-09 23:28:35 -0600

    Change = to +=

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,8 @@
 const reverseString = function(string) {
     let reverseString = null;
 
-
     for (let i = 0; i < string.length; i++) {
-        reverseString = string[string.length - i];
+        reverseString += string[string.length - i];
     }
     return reverseString; 
 };
